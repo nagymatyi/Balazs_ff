5b74d2ea862b0208fe7a2f95544905d054f48270
nagymatyas@stud.uni-obuda.hu <nagymatyas@stud.uni-obuda.hu>
2022-04-15 15:49:09 +0200

    PKMXEN.JSClient creteCarrier done

--- PKMXEN.JSClient/Startup.cs
+++ PKMXEN.JSClient/Startup.cs
@@ -1,13 +1,14 @@
-using PKMXEN.Data;
-using PKMXEN.Logic;
-using PKMXEN.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace PKMXEN.Endpoint
+namespace PKMXEN.JSClient
 {
     public class Startup
     {
@@ -15,17 +16,6 @@ namespace PKMXEN.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-
-            services.AddTransient<ICarrierLogic, CarrierLogic>();
-            services.AddTransient<IOrderLogic, OrderLogic>();
-            services.AddTransient<IParcelLogic, ParcelLogic>();
-            services.AddTransient<ICarrierRepository, CarrierRepository>();
-            services.AddTransient<IOrderRepository, OrderRepository>();
-            services.AddTransient<IParcelRepository, ParcelRepository>();
-            services.AddSingleton<ShippingDbContext, ShippingDbContext>();
-
-            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,18 +26,16 @@ namespace PKMXEN.Endpoint
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseCors(x => x
-                .AllowCredentials()
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .WithOrigins("http://localhost:45495"));
-
             app.UseRouting();
+
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
-                endpoints.MapHub<SignalRHub>("/hub");
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
---
+++ PKMXEN.JSClient/wwwroot/script.js
@@ -0,0 +1,41 @@
+﻿let carriers = [];
+
+getdata();
+
+async function getdata() {
+    await fetch('http://localhost:33503/carrier')
+        .then(x => x.json())
+        .then(y => {
+            carriers = y;
+            console.log(carriers);
+            display();
+        });
+}
+
+function display() {
+    document.getElementById('resultarea').innerHTML = "";
+    carriers.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.carrierID + "</td><td>"
+            + t.name + "</td><td>" + t.age + "</td><td>" + t.salary
+        + "</td><td>" + t.totalNumberOfParcels + "</td><tr>";
+    });
+}
+
+function createCarrier() {
+    let Name = document.getElementById('name').value;
+    let Age = document.getElementById('age').value;
+    let Salary = document.getElementById('salary').value;
+    let TotalNumberOfParcels = document.getElementById('tnop').value;
+    fetch('http://localhost:33503/carrier', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { name: Name, age: Age, salary: Salary, totalNumberOfParcels: TotalNumberOfParcels })})
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+}
